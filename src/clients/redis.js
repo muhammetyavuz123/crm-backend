e04f835a8e6d5b793a87fd3f52d296e958d685e0
muhammetyavuz123 <muhametyavuz@hotmail.com>
2022-06-03 11:45:25 +0300

    asd

--- src/clients/redis.js
+++ src/clients/redis.js
@@ -1,4 +1,7 @@
 const Redis = require("ioredis");
-const redis = new Redis();
+const redis = new Redis(
+  +(process.env.Redis_PORT ?? "6379"),
+  process.env.Redis_HOST ?? "localhost"
+);
 
 export default redis;
